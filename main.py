d494f4f98979bd06d246434e9660a0b07d9f32f0
Richard Neri <richardmatq@gmail.com>
2022-12-11 16:10:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import sqlite3
 from pathlib import Path
 from abc import ABC, abstractmethod
+from os import system
+from sys import platform
 
-DIR = Path(__file__).parent
+def clear():
+    match platform:
+        case 'win32' | 'cygwin':
+            return system('cls')
+        case _:
+            return system('clear')
+
+DIR = Path(__file__).parent / 'data'
+
+DIR.mkdir() if not DIR.exists() else ...
+    
 DBDIRECTORY = DIR / 'database.db'
 
 class RegistrationLogin(ABC):
@@ -21,8 +33,7 @@ class RegistrationLogin(ABC):
     
     @abstractmethod
     def login(self) -> bool:...
-    
-    
+      
 class LoginSystem(RegistrationLogin):
        
     def registration(self) -> bool:
@@ -31,15 +42,51 @@ class LoginSystem(RegistrationLogin):
         
         self.cursor.execute(f'INSERT INTO users VALUES ("{user}","{password}")')
         self.database.commit()
-        self.database.close()
         return True
         
-
     def login(self) -> bool:
-        ...
-    
-usr = input('User: ')
-pwd = input('Password: ')
+        user = self._user
+        password = self._password
+        
+        self.cursor.execute('SELECT * FROM users')
+        for users in self.cursor.fetchall():
+            if user == users[0] and password == users[1]:
+                return True
+            
+        return False
+            
+        
+user = None
+password = None
+system_ = LoginSystem
 
-system = LoginSystem(usr, pwd)
-system.registration()
+while True:
+    option = input('1) Registration\n2) Login\n\nType Here: ')
+    match option:
+        case '1':  # Registration
+            clear()
+            user = input('User: ')
+            password = input('Password: ')
+            system_(user, password).registration()
+            clear()
+            print('Registration Success!\n')
+        case '2':  # Login
+            clear()
+            user = input('User: ')
+            password = input('Password: ')
+            
+            if system_(user, password).login():
+                clear()
+                print('Login Success')
+                break
+            
+            clear()
+            print('Login Failed\n')
+        case _:    # does not exist in option
+            clear()
+            print('No Understand you option! Try Again!\n')
+            
+
+    
+    
+    
